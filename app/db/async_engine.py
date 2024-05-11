8eb543201ad2b7bff7a80ef1fa52d09d2cdfeb8a
Alexandr <pokertrend@yandex.ru>
2024-05-11 17:41:45 +0400

    test connect db

--- app/db/async_engine.py
+++ app/db/async_engine.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from dotenv import load_dotenv
 from sqlalchemy.orm import sessionmaker
@@ -14,6 +15,23 @@ load_dotenv()
 
 db_url = f"postgresql+asyncpg://{db_user}:{db_password}@{db_host}/{db_name}"
 
-engine = create_async_engine(db_url, echo=True)
-async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
+def connect_to_database(db_url, max_retries=10, retry_interval=1):
+    retry_count = 0
+    connected = False
+    engine = None
+
+    while not connected and retry_count < max_retries:
+        try:
+            engine = create_async_engine(db_url, echo=True)
+            connection = engine.connect()
+            connection.close()
+            connected = True
+            print("Connected to database")
+        except Exception as e:
+            print(f"Filed to connection to the database {e}")
+            retry_count += 1
+            time.sleep(retry_interval)
+
+engine = connect_to_database(db_url)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
